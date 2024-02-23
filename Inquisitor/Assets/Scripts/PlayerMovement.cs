6732aae277d016a542f7f0d990752dbee0b6e826
DVMoya <al415438@uji.es>
2024-02-23 11:51:40 +0100

    Smooth
    Smoother movement, smoother camera, smoother gameplay.
    Added a collision box for future attack.
    Attack with K.

--- Inquisitor/Assets/Scripts/PlayerMovement.cs
+++ Inquisitor/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,47 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f; // Velocidad de movimiento del jugador
     public float rotationSpeed = 100f; // Velocidad de rotación del jugador
     public float jumpForce = 10f; // Fuerza del salto
+    public GameObject damageBox;
+
+    public Rigidbody m_Rigidbody;
     private bool isGrounded; // Variable para controlar si el jugador está en el suelo
+
+    void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        damageBox.SetActive(false);
+    }
 
     void Update()
     {
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            m_Rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        // Ataque
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            // Activar el GameObject de daño
+            damageBox.SetActive(true);
+        }
+        else if (Input.GetKeyUp(KeyCode.K))
+        {
+            // Desactivar el GameObject de daño
+            damageBox.SetActive(false);
+        }
+    }
+
+    
+    void FixedUpdate()
+    {
+        /*
+            He colocado el movimiento en FixedUpdate porque si no no era fluido.
+            Salto y Ataque siguen en update para responder lo más rápido posible a los inputs
+        */
+
         // Movimiento horizontal y vertical
         float moveForward = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(0.0f, 0.0f, moveForward);
@@ -19,13 +56,6 @@ public class PlayerMovement : MonoBehaviour
         // Rotación
         float rotationInput = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // Capturar la entrada de rotación lateral
         transform.Rotate(Vector3.up * rotationInput);
-
-        // Salto
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
     }
 
     // Verificar si el jugador está en el suelo
