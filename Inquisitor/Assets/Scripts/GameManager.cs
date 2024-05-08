cd8f99b1d0600382f8618361822014a512bb508e
DVMoya <al415438@uji.es>
2024-05-08 11:31:07 +0200

    Timer implemented
    No more 'p' create. Islands are now generated automatically.
    
    I need to add a way for the gamemanager to know if all enemies have been defeated or not.
    
    Still having issues animating the demon.

--- Inquisitor/Assets/Scripts/GameManager.cs
+++ Inquisitor/Assets/Scripts/GameManager.cs
@@ -1,27 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject LevelController;
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private float timerTime;
+    private float timeElapsed;
+    private int minutes, seconds, cents;
+    private int redZone = 10;
 
     void Start()
     {
         LevelController.SendMessage("CreateLevel");
+        timeElapsed = timerTime;
     }
 
     private void Update()
     {
+        timeElapsed -= Time.deltaTime;
 
-        /*
-         This code must be romved, I'm only using it to test
-         the level genration. Well, it doesn't relly have to be 
-         removed, just change the condition looking for a key 
-         input for something else.
-        */
-        if (Input.GetKeyDown(KeyCode.P)) {
+        if (timeElapsed < 0) timeElapsed = 0;
+        else if (timeElapsed <= redZone)
+        {
+            float pingpong = Mathf.PingPong(Time.time, 1);
+            timerText.color = Color.Lerp(Color.white, Color.red, pingpong);
+            timerText.transform.localScale = Vector3.Lerp(Vector3.one * 1.1f, Vector3.one, pingpong);
+        }
+
+        minutes = (int)(timeElapsed / 60f);
+        seconds = (int)(timeElapsed - minutes * 60f);
+        cents = (int)((timeElapsed - (int)(timeElapsed)) * 100f);
+
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, cents);
+
+        if (timeElapsed == 0) {
             LevelController.SendMessage("CreateLevel");
+            timeElapsed = timerTime;
+            timerText.color = Color.white;
+            timerText.transform.localScale = Vector3.one;
         }
     }
 }
